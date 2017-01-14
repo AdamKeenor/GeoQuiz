93a994ffbde6af22372e9c945c5f3295132b0f46
zedsted <zedsted@gmail.com>
2017-01-14 17:26:25 +0000

    Added sphere movement and rigidbody object to script.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public int thrustX = 5;
+    public Rigidbody rb = null;
+
+    // Use this for initialization
+    void Start ()
     {
-	
-	}
+        rb = GetComponent<Rigidbody>();
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        rb.AddForce(thrustX, 0, 0);
+    }
 
     private void FixedUpdate()
     {
