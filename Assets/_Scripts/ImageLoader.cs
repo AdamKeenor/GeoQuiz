05d87f8841fd1967762d6d2afbfbfdf8a56d83f3
AdamKeenor <adam.keenor@hotmail.com>
2017-03-16 22:14:27 +0000

    Re-arranged image loader code into Question Manager.

--- Assets/_Scripts/ImageLoader.cs
+++ Assets/_Scripts/ImageLoader.cs
@@ -5,42 +5,25 @@ using UnityEngine;
 
 public class ImageLoader : MonoBehaviour
 {
-    public Sprite[] countries;
-    public SpriteRenderer s;
-
-    int iC;
-    int i;
+  
     // Use this for initialization
     void Start ()
     {
-        //how are we loading?
-        //what are we loading?
-        //where are we loading from?
-        countries = Resources.LoadAll<Sprite>("");
-
-        s = GetComponent<SpriteRenderer>();
-        iC = countries.Length;
-        i = 0;
-
-        //what are we giving the image to? 
-
 
     }
-	
+
+    //Create static country sprites
+    public static Sprite[] GetCountrySprites()
+    {
+        //what are we loading and where are we loading from?
+        Sprite[] countries = Resources.LoadAll<Sprite>("");
+
+        return countries;
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.anyKeyDown)
-        {
-            if (i < iC)
-            {
-                Debug.Log("i: " + i);
-                Debug.Log("iC: " + iC);
-                Debug.Log("Country: " + countries[i].name);
-                s.sprite = countries[i];
-                i++;
-            }
-        }
 
     }
 }
---
+++ Assets/_Scripts/QuestionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionManager : MonoBehaviour
+{
+    //Create Object
+    SpriteRenderer s;
+    Sprite[] countryImages;
+
+    int totalImages;
+    int currentImage;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //how are we loading?
+        //what are we loading?
+        s = GetComponent<SpriteRenderer>();
+        countryImages = ImageLoader.GetCountrySprites();
+
+        //i is current image
+        totalImages = countryImages.Length;
+        currentImage = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+        if (Input.anyKeyDown)
+        {
+            if (currentImage < totalImages)
+            {
+                Debug.Log("i: " + currentImage);
+                Debug.Log("iC: " + totalImages);
+                Debug.Log("Country: " + countryImages[currentImage].name);
+                s.sprite = countryImages[currentImage];
+                currentImage++;
+            }
+        }
+    }
+}
